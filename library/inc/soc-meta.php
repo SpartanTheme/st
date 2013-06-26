fe100bafdbd13b5568dcceaaadd877a4236eb298
SpartanTheme <hello@spartantheme.com>
2013-06-26 02:41:34 -0400

    upd

--- library/inc/soc-meta.php
+++ library/inc/soc-meta.php
@@ -18,22 +18,22 @@
 
 		<meta name="description" content="<?php spartan_soc_content(); ?>">
 		<meta name="author" content="<?php the_author(); ?>">
+		<link rel="canonical" href="<?php the_permalink() ?>">
 		
 		<meta name="twitter:card" content="summary">
 		<meta name="twitter:site" content="@SpartanTheme">
 		<meta name="twitter:domain" content="<?php spartan_domain(); ?>">
 		<meta name="twitter:creator" content="@SpartanTheme">
-		
 		<meta name="twitter:url" content="<?php the_permalink() ?>">
 		<meta name="twitter:title" content="<?php spartan_page_title(); ?>">
 		<meta name="twitter:description" content="<?php spartan_soc_content(); ?>">
 		<meta name="twitter:image" content="<?php spartan_soc_img(); ?>">
 		
+		<meta property="og:type" content="article">
+		<meta property="og:title" content="<?php spartan_page_title(); ?>">
 		<meta property="og:url" content="<?php the_permalink() ?>">
 		<meta property="og:site_name" content="<?php spartan_domain(); ?>">
-		<meta property="og:title" content="<?php spartan_page_title(); ?>">
 		<meta property="og:description" content="<?php spartan_soc_content(); ?>">
 		<meta property="og:image" content="<?php spartan_soc_img(); ?>">
 		
-<?php endwhile; ?>
-<?php wp_reset_postdata(); // reset the query ?>
+<?php endwhile; wp_reset_postdata(); // reset the query ?>
