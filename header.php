92d3998a801dad9873b05e22b2cf7439745a922a
Spartan <dev@spartantheme.com>
2013-10-08 15:10:03 -0400

    Cleaning

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 
 	</head>
 
-	<body <?php body_class(); ?> itemscope="itemscope" itemtype="http://schema.org/WebPage"> 
+	<body <?php body_class(); ?> itemscope="itemscope" itemtype="http://schema.org/WebPage">
 	<!--<?php spartan_tpl(); ?>-->
 
 		<div  id="page" class="hfeed site">
@@ -18,18 +18,13 @@
 
 				<div id="inner-header" class="wrap clearfix">
 
-					<p class="site-title" itemprop="name" ><a id="logo" class="h1" href="<?php echo home_url(); ?>" itemprop="url" title="<?php echo bloginfo('name'); ?>"><?php bloginfo( 'name' ); ?></a></p>	
-					<span id="slogan"><?php bloginfo('description'); ?></span>
-					
-					<a href="https://twitter.com/share" class="twitter-share-button" data-size="large">Tweet</a>
-					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-					
-					<?php spartan_socs(); ?>
+					<p class="site-title" itemprop="name" ><a id="logo" class="h1" href="<?php echo home_url(); ?>" itemprop="url" title="<?php echo bloginfo('name'); ?> - <?php bloginfo('description'); ?>"><?php bloginfo('name'); ?></a></p>
 
 				</div><!-- #inner-header -->
-				
+
 				<nav role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
-					<?php spartan_main_nav(); // this prints the navigation ?>
+					<?php spartan_main_nav(); // this prints the navigation ?> <?php spartan_socs(); ?>
 				</nav>
 
 			</header>
+
