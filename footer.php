0bdeebf16bb3f145abfd1a7f0c732452ac0b2f11
Spartan <dev@spartantheme.com>
2013-10-09 11:39:31 -0400

    S

--- footer.php
+++ footer.php
@@ -2,6 +2,10 @@
 
 				<div class="footer-inr wrap clearfix">
 
+					<nav role="navigation">
+						<?php spartan_footer_links(); ?>
+					</nav>
+
 					<?php spartan_socs(); ?>
 
 					<p class="source-org copyright">&copy; 2011 - <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. Designed by <a href="http://wayruro.com">Wayruro</a> &amp; Powered by Wordpress</p>
