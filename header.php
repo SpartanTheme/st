369a9aab896c766b91c328cbca0b544e74d08b0d
Spartan <dev@spartantheme.com>
2013-10-08 20:00:27 -0400

    S

--- header.php
+++ header.php
@@ -12,19 +12,18 @@
 	<body <?php body_class(); ?> itemscope="itemscope" itemtype="http://schema.org/WebPage">
 	<!--<?php spartan_tpl(); ?>-->
 
-		<div  id="page" class="hfeed site">
+		<div id="page">
 
-			<header class="site-header" role="banner" itemscope="itemscope" itemtype="http://schema.org/Organization">
+			<header class="header" role="banner" itemscope="itemscope" itemtype="http://schema.org/Organization">
 
-				<div id="inner-header" class="wrap clearfix">
+				<div class="header-inr wrap clearfix">
 
 					<p class="site-title" itemprop="name" ><a id="logo" class="h1" href="<?php echo home_url(); ?>" itemprop="url" title="<?php echo bloginfo('name'); ?> - <?php bloginfo('description'); ?>"><?php bloginfo('name'); ?></a></p>
 
-				</div><!-- #inner-header -->
-
-				<nav role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
+					<nav class="navigation wrap clearfix" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
 					<?php spartan_main_nav(); // this prints the navigation ?> <?php spartan_socs(); ?>
 				</nav>
 
-			</header>
+				</div><!-- #inner-header -->
 
+			</header>
