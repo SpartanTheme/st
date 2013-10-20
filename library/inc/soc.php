fbf4b1066a97d2dee16c4f70e44112191e815ec0
SpartanTheme <hello@spartantheme.com>
2013-10-19 20:06:58 -0400

    upd

--- library/inc/soc.php
+++
@@ -1,6 +0,0 @@
-<ul class='socs'>
-	<li class='facebook'><a class="glyphicon" href='#' target="_blank" title="<?php echo bloginfo('name'); ?> on Facebook"></a></li>
-	<li class='twitter'><a class="glyphicon" href='#' target="_blank" title="<?php echo bloginfo('name'); ?> on Twitter"></a></li>
-	<li class='gplus'><a class="glyphicon" href='#' target="_blank" title="<?php echo bloginfo('name'); ?> on Google Plus"></a></li>
-	<li class='youtube'><a class="glyphicon" href='#' target="_blank" title="<?php echo bloginfo('name'); ?> on YouTube"></a></li>
-</ul>
